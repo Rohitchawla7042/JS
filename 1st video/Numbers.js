9999489db68e34a12610e71c67e1b169f0e249d3
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-07-26 12:23:00 +0530

    Basic One
    Description of let, const,var , opeartors , etc

---
+++ 1st video/Numbers.js
@@ -0,0 +1,27 @@
+console.log("Rohit")
+//varible and let 
+
+//variables are like labelled jars 
+
+let age = 72
+
+console.log(age)
+
+let score  = 0;
+
+//const 
+
+const age1 = 18;
+
+// As now this const value will not change 
+
+//in let we can change value but in const we cant 
+
+//var 
+
+//var is the oldest way 
+
+
+let b = 10; 
+b +=20;
+b--;
